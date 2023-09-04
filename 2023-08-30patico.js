a26ec43dd1e5fe180092690213fb3c5f01794a79
MoonlessŃight <brianariza101mail.com>
2023-09-04 11:12:36 -0500

    metodo para re-asignar atributos

--- 2023-08-30patico.js
+++ 2023-08-30patico.js
@@ -7,4 +7,4 @@ input=document.querySelector('input');console.log(input.value);console.log({h1,p
 h1.innerHTML = 'Patito <br> Feo';
 h1.innerText = 'Patito <br> Feo';
 console.log(h1.getAttribute('class'));
-h1.setAttribute
+h1.setAttribute('class','rojo');
