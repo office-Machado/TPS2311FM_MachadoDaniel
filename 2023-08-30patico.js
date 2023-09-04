860ed206a77b200b7add51f1c5f48d48412149fe
MoonlessŃight <brianariza101mail.com>
2023-09-04 10:49:17 -0500

    inyectando texto plano

--- 2023-08-30patico.js
+++ 2023-08-30patico.js
@@ -4,4 +4,5 @@ parrafito=document.getElementsByClassName('.parrafito');const
 pid=document.getElementById('#pid');const  
 input=document.querySelector('input');console.log(input.value);console.log({h1,p,parrafito,pid,input,});
 
-h1.innerHTML = 'Patito <br> feo';
+h1.innerHTML = 'Patito <br> Feo';
+h1.innerText = 'Patito <br> Feo';
