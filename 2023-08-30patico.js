c534cdd94e774549700722f08a74c75050f59fe1
MoonlessŃight <brianariza101mail.com>
2023-09-06 12:22:27 -0500

    06&09&2023

--- 2023-08-30patico.js
+++ 2023-08-30patico.js
@@ -2,9 +2,12 @@ const h1=document.querySelector('h1');
 const input1=document.querySelector('#calculo1');
 const input2=document.querySelector('#calculo2');
 const btn=document.querySelector('#btncalcular');
+const pResult=document.querySelector('#result');
+
 
 function btnOnclick () {
-    console.log(input1.value + input2.value);
+    const sumaInputs=  input1.value + input2.value ;
+    pResult.innerText = "Resultado:"+ sumaInputs;
 }
 
 
