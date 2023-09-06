3eaf3525111a63058342fdce0d58ac1204560e95
MoonlessŃight <brianariza101mail.com>
2023-09-06 11:24:23 -0500

    MANIPULACION DE INPUTS

--- 2023-08-30patico.js
+++ 2023-08-30patico.js
@@ -1,10 +1,11 @@
-
 const h1=document.querySelector('h1');
 const input1=document.querySelector('#calculo1');
 const input2=document.querySelector('#calculo2');
 const btn=document.querySelector('#btncalcular');
 
-
+function btnOnclick () {
+    console.log(input1.value + input2.value);
+}
 
 
 
