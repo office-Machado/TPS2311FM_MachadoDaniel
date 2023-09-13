1124e1a77ed91bdc6b90746abe292fc804696afd
diego <diegoalexanderpi1@gmail.com>
2023-09-13 11:28:12 -0500

    destructuring en javascript

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('peticion GET');
+app.get('/:name/:id',(req,res)=>{
+    const {name,id}=req.params;
+
+
+
+    res.send('${name}:${id}');
 });
+
 app.post('/',(req,res)=>{
     res.send('Peticion POST');
 });
+
+app.put('/',(req,res)=>{
+    res.send('Peticion PUT');
+});
+
+app.delete('/',(req,res)=>{
+    res.send('Peticion DELETE');
+});
+
 app.listen(3000,() =>{
     console.log('Servidor encendido');
 });
