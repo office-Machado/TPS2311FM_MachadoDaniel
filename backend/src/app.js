4aa341b02237e5aa816a7e51ffce03e3ebb60b08
diego <diegoalexanderpi1@gmail.com>
2023-09-13 11:38:55 -0500

    pasar informacion por el body

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get('/:name/:id',(req,res)=>{
     const {name,id}=req.params;
 
@@ -11,7 +14,8 @@ app.get('/:name/:id',(req,res)=>{
 });
 
 app.post('/',(req,res)=>{
-    res.send('Peticion POST');
+    const{email,password }=req.body;
+    res.send('${email}:${password}');
 });
 
 app.put('/',(req,res)=>{
