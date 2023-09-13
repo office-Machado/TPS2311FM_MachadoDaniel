400cda83f30541d95cb01bf1de5b0f6537df55f7
diego <diegoalexanderpi1@gmail.com>
2023-09-13 10:09:32 -0500

    primer ruta/endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.send('peticion GET');
+});
+app.post('/',(req,res)=>{
+    res.send('Peticion POST');
+});
 app.listen(3000,() =>{
     console.log('Servidor encendido');
 });
